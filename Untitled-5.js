97293548045f861d3bcc9a2693134a18e1f8ae09
MaheenShahid785 <145053011+MaheenShahid785@users.noreply.github.com>
2024-01-13 11:47:46 +0500

    Add files via upload

---
+++ Untitled-5.js
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <title>Desktop - 5</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter%3A400"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400"/>
+  <link rel="stylesheet" href="./styles/desktop-5.css"/>
+</head>
+<body>
+<div class="desktop-5-jX3">
+  <div class="desktop-1-H2m">
+    <div class="desktop-1-Eyb">
+      <div class="auto-group-lxdf-zhs">
+        <div class="rectangle-1-jfT">
+        </div>
+        <p class="hotel-management-system-5UR">HOTEL MANAGEMENT SYSTEM</p>
+        <div class="rectangle-6-BXT">
+        </div>
+        <p class="function-list-UWZ">
+        Function 
+        <br/>
+        list
+        </p>
+        <p class="employee-info-wQ9">
+        Employee
+        <br/>
+        Info
+        </p>
+        <p class="management-Eu3">Management</p>
+        <p class="reserve-and-checkout-Yeq">
+        Reserve 
+        <br/>
+        and
+        <br/>
+        Checkout
+        </p>
+        <p class="room-info-management-r9j">
+        Room 
+        <br/>
+        Info
+        <br/>
+        Management
+        </p>
+        <p class="customer-info-query-MMP">
+        Customer
+        <br/>
+        Info
+        <br/>
+        Query
+        </p>
+        <p class="change-password-1gq">
+        Change
+        <br/>
+        Password
+        </p>
+      </div>
+      <p class="layout-hJm">Layout</p>
+      <div class="auto-group-5rsh-2rq">
+        <div class="auto-group-gn9b-knq">Reservation</div>
+        <div class="auto-group-ygaz-RPB">
+          <div class="auto-group-4wwb-xe1">
+            <img class="rectangle-5-9Ch" src="./assets/rectangle-5-XeV.png"/>
+            <div class="rectangle-8-3Yy">
+            </div>
+            <p class="room-number-customer-name-phone-number-e-mail-current-time-note--BfB">
+            Room Number:
+            <br/>
+            Customer Name:
+            <br/>
+            Phone Number:
+            <br/>
+            E-mail:
+            <br/>
+            Current Time:
+            <br/>
+            Note:
+            </p>
+          </div>
+        </div>
+        <div class="auto-group-3f6u-FQ9">
+          <div class="auto-group-wbft-AGD">
+            <p class="return-V3b">Return</p>
+            <img class="line-1-aqj" src="./assets/line-1-nNh.png"/>
+          </div>
+          <div class="auto-group-cmed-hvM">Checkout</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
